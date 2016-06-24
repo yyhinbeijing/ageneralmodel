5542b0f0b386f56e63c7feae3d8c43a1023189d8
阳永辉 <hopeyyh@qq.com>
2016-06-24 11:29:04 +0800

    Initial Commit

---
+++ GeneralDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeneralDemo
+//
+//  Created by 阳永辉 on 16/6/24.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeneralDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GeneralDemo
+//
+//  Created by 阳永辉 on 16/6/24.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
